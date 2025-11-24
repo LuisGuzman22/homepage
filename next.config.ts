13a3752e7ec7a37d00fdea1056867849799a68f9
Luis Guzman <lguzman2207@gmail.com>
2025-11-23 22:37:17 -0300

    change next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  distDir: "build", // This will output to 'build' instead of '.next'
 };
 
 export default nextConfig;
